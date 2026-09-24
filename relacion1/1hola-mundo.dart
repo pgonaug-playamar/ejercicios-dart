32a2594eb5eeb536e7a3d872432ede172a677acd
“Pilar <tengo.una.vida.nueva@gmail.com>
2026-09-24 19:25:48 +0200

    ejercicio uno, probando dart

---
+++ relacion1/1hola-mundo.dart
@@ -0,0 +1,12 @@
+/* Ejercicio 1 de la relación 1
+   archivo: 1hola-mundo.dart
+   fecha creación: septiembre 2026
+   autora: Pilar González */
+
+void main(){
+    
+    String nombre = 'Pilar'; // tipo y nombre de la variable
+    int veces = 1;
+    print ('Hola $nombre!'); //la variable con dolar delante dentro del print muestra su valor
+    print ('has ejecutado el programa ${veces*2-1} vez(es)');// si es una expresión se pone entre llaves
+}
